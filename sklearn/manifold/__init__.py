502c1f42bc8e2f0c84f86e3c702ac120d35c868e
Noah D. Brenowitz <nbren12@gmail.com>
2013-06-30 12:20:54 -0400

    fixed import

--- sklearn/manifold/__init__.py
+++ sklearn/manifold/__init__.py
@@ -5,7 +5,7 @@ The :mod:`sklearn.manifold` module implements data embedding techniques.
 from .locally_linear import locally_linear_embedding, LocallyLinearEmbedding
 from .isomap import Isomap
 from .mds import MDS
-from .spectral_embedding import (SpectralEmbedding, spectral_embedding,
+from .spectral_embedding_ import (SpectralEmbedding, spectral_embedding,
                                  DiffusionEmbedding)
 
 __all__ = ['locally_linear_embedding', 'LocallyLinearEmbedding', 'Isomap',
